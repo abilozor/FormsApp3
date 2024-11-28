b23463ba143aa05b4e9f080f529958280f250fa8
Artem Bilozor <abilozor0@gmail.com>
2024-11-28 09:36:58 +0200

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FormsApp2
+{
+    public class Car
+    {
+        public string ModelName { get; set; }
+        public string Manufacturer { get; set; }
+        public int YearOfProduction { get; set; }
+        public double EngineCapacity { get; set; }
+        public int SeatCount { get; set; }
+        public bool IsElectric { get; set; }
+        public double AverageFuelConsumption { get; set; }
+
+        public Car()
+        { }
+
+        public Car(string modelName, string manufacturer, int yearOfProduction, 
+            double engineCapacity, int seatCount, bool isElectric, double averageFuelConsumption)
+        {
+            ModelName = modelName;
+            Manufacturer = manufacturer;
+            YearOfProduction = yearOfProduction;
+            EngineCapacity = engineCapacity;
+            SeatCount = seatCount;
+            IsElectric = isElectric;
+            AverageFuelConsumption = averageFuelConsumption;
+        }
+
+        public double GetFuelConsumptionPer100Km()
+        {
+            return AverageFuelConsumption;
+        }
+
+        public int GetCarAge()
+        {
+            return DateTime.Now.Year - YearOfProduction;
+        }
+    }
+}
